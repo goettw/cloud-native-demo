76d9e47f46735aa1cfb2a60bebe08f5308d51f71
Goette <Wolfgang.Goette@emc.com>
2017-06-23 13:23:16 +0200

    fixes to run and support cf push

--- reservation-service/src/main/java/com/example/demo/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/demo/ReservationServiceApplication.java
@@ -1,12 +1,94 @@
 package com.example.demo;
 
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 
 @SpringBootApplication
-public class ReservationServiceApplication {
+public class ReservationServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+	
+	
+	private final ReservationRepository reservationRepository;	
+	
+	
+
+	
+	public ReservationServiceApplication(ReservationRepository reservationRepository) {
+
+		this.reservationRepository = reservationRepository;
+	}
+
+
+
+		@Override
+		public void run(String... strings) throws Exception {
+			Stream.of("Wolfgang,Sabine,Luise,Tabea".split(","))
+			
+			.forEach(name -> reservationRepository.save(new Reservation(name)));
+			
+			reservationRepository.findAll().forEach(System.out::println);
+		}
+
+	
+	
+
+}
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation,Long> {
+	@RestResource(path = "by-name")
+	Collection<Reservation> findByReservationName(@Param("rn") String rn);
+
+}
+
+
+@Entity
+
+class Reservation {
+
+	public Reservation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String reservationName;
+
+	public Reservation(String rn) {
+		this.reservationName = rn;
 	}
 }
